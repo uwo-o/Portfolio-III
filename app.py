face0fcd9b0463cc396569d2f758c5e85667cefc
uwo-o <hugo.campos@usm.cl>
2021-11-15 18:46:47 -0300

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
 import smtplib
 from decouple import config
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app=Flask(__name__)
 app.secret_key = 'some_secret'
