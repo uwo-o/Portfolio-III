0bb9e0fa01a5abe72ba515b39e4750cc15819c67
uwo-o <hugo.campos@usm.cl>
2021-12-12 23:45:12 -0300

    micro changes

--- static/js/scheme.js
+++
@@ -1,9 +0,0 @@
-{
-    "@context": "https://schema.org/",
-    "@type": "Person",
-    "name": "Hugo Fernando Campos Castro",
-    "url": "http://uwo.pythonanywhere.com/",
-    "image": "http://uwo.pythonanywhere.com/static/img/IMG_20210904_222817.png",
-    "sameAs": "http://uwo.pythonanywhere.com/",
-    "jobTitle": "Front end developer"  
-  }
